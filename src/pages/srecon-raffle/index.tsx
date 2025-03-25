d6c56f9525a443e68e9c3a70af0f4282c9ad0d1f
Philip Miglinci <pmig@glasskube.com>
2025-03-25 10:30:38 -0700

    feat: add raffle details and use forms backend
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- src/pages/srecon-raffle/index.tsx
+++ src/pages/srecon-raffle/index.tsx
@@ -2,7 +2,12 @@ import Heading from '@theme/Heading';
 import React from 'react';
 import styles from './index.module.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faGift, faCalendar, faEnvelope, faSmile, faHandHoldingHeart} from '@fortawesome/free-solid-svg-icons';
+import {
+  faCalendar,
+  faEnvelope,
+  faGift,
+  faHandHoldingHeart,
+} from '@fortawesome/free-solid-svg-icons';
 import ThemedImage from '@theme/ThemedImage';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from '@theme/Layout';
@@ -35,21 +40,21 @@ function loadScript() {
   }
 }
 
-interface RaffleEntry {
-  firstname: string;
-  lastname: string;
+interface RaffleRequest {
+  firstName: string;
+  lastName: string;
   email: string;
-  jobtitle: string;
-  company: string;
-  onprem_customers: string;
+  jobTitle: string;
+  companyName: string;
+  onPremCustomers: string;
 }
 
-class Form extends React.Component<unknown, {data: RaffleEntry}> {
+class Form extends React.Component<unknown, {data: RaffleRequest}> {
   hubSpotLoaded = false;
 
   constructor(props: unknown) {
     super(props);
-    this.state = {data: {} as RaffleEntry};
+    this.state = {data: {} as RaffleRequest};
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -66,34 +71,23 @@ class Form extends React.Component<unknown, {data: RaffleEntry}> {
 
     try {
       const response = await fetch(
-        'https://forms.glasskube.com/api/v1/srecon-raffle',
+        'https://forms.glasskube.com/api/v1/raffle',
         {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
           },
-          body: JSON.stringify({
-            fields: [
-              { name: "firstname", value: this.state.data.firstname },
-              { name: "lastname", value: this.state.data.lastname },
-              { name: "email", value: this.state.data.email },
-              { name: "jobtitle", value: this.state.data.jobtitle },
-              { name: "company", value: this.state.data.company },
-              { name: "onprem_customers", value: this.state.data.onprem_customers }
-            ],
-            context: {
-              pageUri: window.location.href,
-              pageName: "SREcon Raffle Entry"
-            }
-          })
-        }
+          body: JSON.stringify(this.state.data),
+        },
       );
 
       if (response.ok) {
-        alert('Thank you! You have been entered into the raffle. Best of luck!');
+        alert(
+          'Thank you! You have been entered into the raffle. Best of luck!',
+        );
         window.location.href = '/';
       } else {
-        throw new Error("Error submitting form to HubSpot");
+        throw new Error('Error submitting form to HubSpot');
       }
     } catch (error) {
       console.error('Error:', error);
@@ -112,10 +106,10 @@ class Form extends React.Component<unknown, {data: RaffleEntry}> {
             id="firstname"
             required
             placeholder="First Name"
-            value={this.state.data.firstname}
+            value={this.state.data.firstName}
             onChange={e =>
               this.setState(state => ({
-                data: {...state.data, firstname: e.target.value},
+                data: {...state.data, firstName: e.target.value},
               }))
             }
           />
@@ -128,10 +122,10 @@ class Form extends React.Component<unknown, {data: RaffleEntry}> {
             id="lastname"
             required
             placeholder="Last Name"
-            value={this.state.data.lastname}
+            value={this.state.data.lastName}
             onChange={e =>
               this.setState(state => ({
-                data: {...state.data, lastname: e.target.value},
+                data: {...state.data, lastName: e.target.value},
               }))
             }
           />
@@ -163,10 +157,10 @@ class Form extends React.Component<unknown, {data: RaffleEntry}> {
             id="jobtitle"
             required
             placeholder="Job Title"
-            value={this.state.data.jobtitle}
+            value={this.state.data.jobTitle}
             onChange={e =>
               this.setState(state => ({
-                data: {...state.data, jobtitle: e.target.value},
+                data: {...state.data, jobTitle: e.target.value},
               }))
             }
           />
@@ -180,28 +174,29 @@ class Form extends React.Component<unknown, {data: RaffleEntry}> {
             id="company"
             required
             placeholder="Company"
-            value={this.state.data.company}
+            value={this.state.data.companyName}
             onChange={e =>
               this.setState(state => ({
-                data: {...state.data, company: e.target.value},
+                data: {...state.data, companyName: e.target.value},
               }))
             }
           />
         </div>
 
         <div>
-          <label htmlFor="onprem_customers">Does your company currently serve on-prem customers?*</label>
+          <label htmlFor="onprem_customers">
+            Does your company currently serve on-prem customers?*
+          </label>
           <select
             className={styles.input}
             id="onprem_customers"
             required
-            value={this.state.data.onprem_customers}
+            value={this.state.data.onPremCustomers}
             onChange={e =>
               this.setState(state => ({
-                data: {...state.data, onprem_customers: e.target.value},
+                data: {...state.data, onPremCustomers: e.target.value},
               }))
-            }
-          >
+            }>
             <option value="">Select an option</option>
             <option value="yes">Yes</option>
             <option value="no">No</option>
@@ -223,28 +218,33 @@ function FeatureList() {
       <li className={styles.featureItem}>
         <FontAwesomeIcon icon={faGift} size="2x" />
         <div>
-          <h4>To celebrate SREcon 2025, we're running a raffle!</h4>
-          <p>Be in for a chance to win three deadly prizes!</p>
+          <h4>To celebrate SREcon 2025, we&#39;re running a raffle!</h4>
+          <p>
+            Be in for a chance to win{' '}
+            <strong>Lego Star Wars Millennium Falcon</strong>
+          </p>
         </div>
       </li>
       <li className={styles.featureItem}>
         <FontAwesomeIcon icon={faCalendar} size="2x" />
         <div>
-          <h4>Location and time to be announced</h4>
-          <p>Sign up and stay tuned for more details about the event.</p>
+          <h4>Thursday, March 27 at 1pm in front of the Ballroom CDEF</h4>
+          <p>You need to be present to win.</p>
         </div>
       </li>
       <li className={styles.featureItem}>
         <FontAwesomeIcon icon={faEnvelope} size="2x" />
         <div>
-          <h4>Sign up with your work email, that's it!</h4>
-          <p>We'll use your email to contact you if you win.</p>
+          <h4>Sign up with your work email, that&#39;s it!</h4>
+          <p>We&#39;ll use your email to contact you if you win.</p>
         </div>
       </li>
       <li className={styles.featureItem}>
         <FontAwesomeIcon icon={faHandHoldingHeart} size="2x" />
         <div>
-          <h4>Swag is nice and all, but how many conference raffles have you won?</h4>
+          <h4>
+            Swag is nice and all, but how many conference raffles have you won?
+          </h4>
           <p>Sign up now and get ready for some fun!</p>
         </div>
       </li>
