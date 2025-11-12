f5fde6cc44de84743186a4e5d2309d31ce9b39b7
Philip Miglinci <pmig@glasskube.com>
2025-11-12 16:36:04 +0100

    chore: distr content migration to distr.sh (#173)
    Signed-off-by: Philip <pmig@glasskube.com>
    Co-authored-by: Louis Weston <lw@glasskube.com>

--- src/theme/NavbarItem/NavbarWrapper/index.tsx
+++ src/theme/NavbarItem/NavbarWrapper/index.tsx
@@ -1,21 +1,12 @@
-import CustomGitHubButton from '@site/src/components/buttons/CustomGitHubButton';
-import DemoButton from '@site/src/components/buttons/DemoButton';
-import SignupForWaitlistButton from '@site/src/components/buttons/SignupForWaitlistButton';
+import TalkToFoundersButton from '@site/src/components/buttons/TalkToFoundersButton';
 import styles from './styles.module.css';
 
 function Index() {
   return (
     <>
-      <div className={styles.wrapper}>
-        <div className={styles.center}>
-          <CustomGitHubButton href="https://github.com/glasskube/distr" />
-        </div>
-      </div>
       <div className={styles.ctaWrapper}>
         <div className={styles.center}>
-          <DemoButton additionalClassNames={''} />
-          &nbsp;&nbsp;
-          <SignupForWaitlistButton additionalClassNames={''} />
+          <TalkToFoundersButton additionalClassNames={''} />
         </div>
       </div>
     </>
